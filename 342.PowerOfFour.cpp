9d42925d21efd0c8255094cde8f451fcf14e458f
ZakirShaik <skz.zakir@gmail.com>
2025-05-25 23:18:57 -0600

    Sol: Leetcode 342.Power of four

---
+++ 342.PowerOfFour.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+// Solution 3: Time Complexity: log n base 4 ~~ (log n base 2)/2 ~~ (log n) ;
+// Space Complexity: O(1)
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 0) return false;
+
+        while(n%4 == 0) n /= 4;
+
+        return n == 1;
+    }
+/*
+Solution 2:  Time Complexity: log n base 4 ~~ (log n base 2)/2 ~~ (log n) ; 
+            Space Complexity: log n base 4 ~~ (log n base 2)/2 ~~ (log n) ;
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 1) return true;
+        else if (n < 4) return false;
+
+        if(n%4 == 0)
+            return isPowerOfFour(n/4);
+
+        return false;
+    }
+*/
+
+/*
+Solution 1:  Time Complexity: log n base 4 ~~ (log n base 2)/2 ~~ (log n) ; Space Complexity: O(1)
+public:
+    bool isPowerOfFour(int num) {
+        while (num > 1) {
+			if (num % 4) return false;
+			num /= 4;
+		}
+		return (num == 1);
+    }
+};
+*/
+
+int main(int argc, const char * argv[]) {
+    Solution sol;
+    cout << sol.isPowerOfFour(40) << endl;
+    return 0;
+}
