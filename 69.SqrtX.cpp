200829a7daa0f0e37e1b04bc64dcaf6570cbb5ad
ZakirShaik <skz.zakir@gmail.com>
2025-05-27 21:20:29 -0600

    Update comment 69 sqrt x leetcode

--- 69.SqrtX.cpp
+++ 69.SqrtX.cpp
@@ -25,24 +25,24 @@ public:
         return end;
     }
 // Solution 1: Time Complexity: O(log n); Space Complexity: O(1)
-// public:
-//    int mySqrt(int x) {
-//        if(x < 2) return x;
-//        long long start = 1, end = x/2, squareRoot = 0;
-//        while(start <= end) {
-//            long long mid = (start + end)/2;  // Here (start + end)/2 causes integer overflow. So, long long is being used. It's better to use start + (end - start)/2 to use int
-//            long long square = mid * mid;
-//            if(square == x) {
-//                squareRoot = mid;
-//                break;
-//            } else if(square > x) end = mid - 1;
-//            else {
-//                squareRoot = max(squareRoot, mid);
-//                start = mid + 1;
-//            }
-//        }
-//        return (int)squareRoot;
-//    }
+public:
+   int mySqrt(int x) {
+       if(x < 2) return x;
+       long long start = 1, end = x/2, squareRoot = 0;
+       while(start <= end) {
+           long long mid = (start + end)/2;  // Here (start + end)/2 causes integer overflow if int is used as datatype. So, long long is being used. It's better to use start + (end - start)/2 to use int
+           long long square = mid * mid;
+           if(square == x) {
+               squareRoot = mid;
+               break;
+           } else if(square > x) end = mid - 1;
+           else {
+               squareRoot = max(squareRoot, mid);
+               start = mid + 1;
+           }
+       }
+       return (int)squareRoot;
+   }
 };
 
 int main(int argc, const char * argv[]) {
